51edb06a865e2fd924242d58651dd0566879bd4f
Rudolph Newball <newrdev@gmail.com>
2015-06-27 16:20:34 -0500

    Initial AngularJS + Gulp setup

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var refresh = require('gulp-livereload'),
+    lrserver = require('tiny-lr')(),
+    express = require('express'),
+    livereload = require('connect-livereload'),
+    livereloadport = 35729,
+    serverport = 5000;
+
+// Set up an express server (but not starting it yet)
+var server = express();
+// Add live reload
+server.use(livereload({port: livereloadport}));
+// Use our 'dist' folder as rootfolder
+server.use(express.static('./dist'));
+// Because I like HTML5 pushstate .. this redirects everything back to our index.html
+server.all('/*', function(req, res) {
+  res.sendfile('index.html', { root: 'dist' });
+});
+
+// Dev task
+gulp.task('dev', function() {
+  // Start webserver
+  server.listen(serverport);
+  // Start live reload
+  lrserver.listen(livereloadport);
+  // // Run the watch task, to keep taps on changes
+  // gulp.run('watch');
+});
