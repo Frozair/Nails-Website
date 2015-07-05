13fa167d9453110f36d5d082ebcc4f5f60c62f2e
Rudolph Newball <newrdev@gmail.com>
2015-07-04 21:17:30 -0500

    Sidenav is now on index.html due to it not hiding the overflow done on the body element

---
+++ client/app/layout/sidebar.controller.js
@@ -0,0 +1,73 @@
+(function(){
+  'use-strict';
+
+  angular
+    .module('app.layout')
+    .controller('SideBarController', SideBarController);
+
+  /* @ngInject */
+  function SideBarController($mdSidenav, $window, $state){
+    var ctrl = this;
+    ctrl.socialList = getSocialList();
+    ctrl.navRoutes = getNavRoutes();
+    ctrl.open = openLink;
+    ctrl.navigateTo = navigateToFunc;
+
+    function getSocialList(){
+      return [
+        {
+          name: 'Contact',
+          img: '/images/social/email.png',
+          href: '#'
+        },
+        {
+          name: 'Twitter',
+          img: '/images/social/twitter.png',
+          href: 'https://twitter.com/theangelmarie'
+        },
+        {
+          name: 'Instagram',
+          img: '/images/social/instagram.png',
+          href: 'https://instagram.com/theangelmarie'
+        }
+      ];
+    }
+
+    function getNavRoutes() {
+      return [
+        {
+          name: 'Home',
+          state: 'home'
+        },
+        {
+          name: 'Gallery',
+          state: 'gallery'
+        },
+        {
+          name: 'Prices',
+          state: 'gallery'
+        },
+        {
+          name: 'Schedule (Coming Soon)',
+          state: ''
+        },
+        {
+          name: 'About',
+          state: 'gallery'
+        }
+      ];
+    }
+
+    function openLink(url){
+      if(url != '#')
+        $window.open(url);
+    }
+
+    function navigateToFunc(index){
+      var state = ctrl.navRoutes[index].state;
+      if(state !== '')
+        $state.go(state);
+      $mdSidenav('left').close();
+    }
+  }
+})();
--- client/app/layout/topbar.controller.js
+++ client/app/layout/topbar.controller.js
@@ -9,36 +9,9 @@
   function TopBarController($mdSidenav, $window){
     var ctrl = this;
     ctrl.toggle = toggleSideNav;
-    ctrl.socialList = getSocialList();
-    ctrl.open = openLink;
 
     function toggleSideNav(){
       $mdSidenav('left').toggle();
-    }
-
-    function getSocialList(){
-      return [
-        {
-          name: 'Contact',
-          img: '/images/social/email.png',
-          href: '#'
-        },
-        {
-          name: 'Twitter',
-          img: '/images/social/twitter.png',
-          href: 'https://twitter.com/theangelmarie'
-        },
-        {
-          name: 'Instagram',
-          img: '/images/social/instagram.png',
-          href: 'https://instagram.com/theangelmarie'
-        }
-      ];
-    }
-
-    function openLink(url){
-      if(url != '#')
-        $window.open(url);
     }
   }
 
