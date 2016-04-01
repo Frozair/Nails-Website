a55b4c3e87c24e9f6c398d6caaa29ca05921684c
Rudolph Newball <newrdev@gmail.com>
2016-03-31 21:55:36 -0500

    Gallery thumbnails optimization. Gallery itself needs work, swiping through pictures is very laggy.

--- client/app/gallery/gallery.controller.js
+++ client/app/gallery/gallery.controller.js
@@ -6,9 +6,8 @@
     .controller('GalleryController', GalleryController);
 
   /* @ngInject */
-  function GalleryController(GalleryDialog, $http){
-    var TOTAL_IMAGES = 33;
-
+  function GalleryController($http)
+  {
     var ctrl = this;
     ctrl.images = [];
     ctrl.openSlideshowModal = openModal;
@@ -47,6 +46,9 @@
       } else {
         ratio = window.innerHeight / window.innerWidth;
       }
+      console.log(ratio);
+
+      var url = "//res.cloudinary.com/angelmarie/image/upload";
 
       angular.forEach(response.data.resources, function(value, key) {
         var w, h;
@@ -60,11 +62,16 @@
           h = value.width * ratio;
         }
 
+        // w = 500;
+        // h = 500;
+        var thumbUrl = url + "/w_300,h_300,c_fill/v" + value.version + "/" + value.public_id + "." + value.format;
+        // var srcUrl   = url + "/";
+
         var img = {
           src: value.url,
           w: w,
           h: h,
-          thumbUrl: value.url
+          thumbUrl: thumbUrl
         };
 
         this.push(img);
